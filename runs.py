b91a1635c1ac1cd31213e2da694d0fc0d006f97f
smldub <smlwarren156@gmail.com>
2024-07-08 15:31:29 -0500

    documentation for the runs.py file

--- runs.py
+++ runs.py
@@ -1,10 +1,24 @@
+'''
+Importing some basic packages
+'''
+
 import numpy as np
 import numpy.linalg as LA
 from functools import reduce
 from pyscf import gto, scf
-from QEDHF import qedhf
 
+'''
+Importing some custom packages that contain QED code and ACSE objects
+'''
+
+from QEDHF import qedhf
 from ACSE_quant import ACSE
+
+'''
+Now we are naming a simulator object
+We have commented out the runtime backend, and are using the AerSimulator
+You could also use a fakebackend as indicated
+'''
 
 #from qiskit_ibm_runtime import QiskitRuntimeService
 # service = QiskitRuntimeService(
@@ -20,6 +34,11 @@ sim = AerSimulator()
 #sim = FakeLagosV2()
 
 
+'''
+Here we are defining the dissociation range for H2
+and some lists to store our energy, 1-RDMs, and expected boson populations
+'''
+
 ran = np.linspace(.5,2.5,7)
 en = []
 rdm = []
@@ -27,6 +46,9 @@ boson_pop = []
 
 
 for ri,r in enumerate(ran):
+        '''
+        Creating a Pyscf Hartree Fock Obj
+        '''
         mol = gto.M(atom = f"H 0 0 0; H 0 0 {r}",
                 basis = "sto3g",
                 unit = "A",
@@ -35,13 +57,29 @@ for ri,r in enumerate(ran):
         mf.kernel()
         myhf = scf.HF(mol)
         myhf.kernel()
+
+        '''
+        Creating a QED-HF object, need to specify the lambda vector
+        '''
         lamb = np.array([0.0,0.0,0.2])
         myqed  = qedhf(myhf,lamb)
         myqed.kernel()
+
+        '''
+        Starting the ACSE
+        ACSE(QEDHF_obj,boson_excitions,['fermionic_residual','mixed_residual'])
+        myacse.delta = the forward and backward time propogation amount for 2-RDM calculation
+        myacse.sim = the above specified simulator
+        myacse.run(number of steps in the ACSE)
+        '''
         myacse = ACSE(myqed,1, ['ferm2A','mix1A'])
         myacse.delta = .5
         myacse.sim = sim
         myacse.run(5)
+
+        '''
+        Collecting the results!
+        '''
         en.append(np.min(myacse.e))
         rdm.append(myacse.rdm[np.argmin(myacse.e)][0])
         boson_pop.append(myacse.rdm[np.argmin(myacse.e)][1][0])
